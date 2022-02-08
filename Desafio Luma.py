4c377ebc3762567cc7444f3c81fe28a032660e6e
luizahage <luiza.t.hage@gmail.com>
2022-02-08 09:30:22 -0300

    Criação das funções

--- Desafio Luma.py
+++ Desafio Luma.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# ### Pontos de Interesse por GPS - Integração Banco de Dados
+# ### Pontos de Interesse por GPS - Integração Funções
 
 # In[102]:
 
@@ -44,31 +44,60 @@
 # print(dict_poi["name"])
 
 
-# In[19]:
+# In[63]:
 
 
 from math import sqrt 
 
-def calc_dist(xa, ya, xb, yb):
-    return sqrt(((xa - xb)**2) + ((ya - yb)**2))
+class Poi:
+    def __init__(self, name, x, y):
+        self.name = name
+        self.x = x
+        self.y = y
+        
+    def __str__(self):
+        return '{} (x = {}, y = {})'.format(self.name, self.x, self.y)
+        
+        
+    def calc_distance(self, x, y):
+        return sqrt(((x - self.x)**2) + ((y - self.y)**2))
 
-def insert_multiple_pois(connection, list_pois):
+# def format_poi(poi):
+#     return '{} (x = {}, y = {})'.format(poi['name'], poi['x'], poi['y'])
+
+# def create_poi(name, x, y):
+#     return {'name': name, 'x': x, 'y': y}
+
+#def calc_distance(xa, ya, xb, yb):
+#        return sqrt(((xa - xb)**2) + ((ya - yb)**2))   
+        
+def insert_pois(connection, pois):
     cursor = connection.cursor()
-    for poi in list_pois:
-        cursor.execute("INSERT INTO POI (NAME, X, Y) VALUES (?, ?, ?)", (poi['name'], poi['x'], poi['y']))
+    
+    if not isinstance(pois, list):
+        pois = [pois]
+    
+    for poi in pois:
+        cursor.execute("INSERT INTO POI (NAME, X, Y) VALUES (?, ?, ?)", (poi.name, poi.x, poi.y))
+#        cursor.execute("INSERT INTO POI (NAME, X, Y) VALUES (?, ?, ?)", (poi['name'], poi['x'], poi['y']))
+
     cursor.commit()
     cursor.close()
 
 def get_pois(connection):
     cursor = connection.cursor()
     cursor.execute("SELECT * FROM POI") 
-    row = cursor.fetchall()
+    rows = cursor.fetchall()
+    pois = []
+    for id_, name, x, y in rows:
+#        pois.append({'name': name, 'x': x, 'y': y})
+        pois.append(Poi(name, x, y))
     cursor.close()
     
-    return row
+    return pois
 
 
-# In[1]:
+# In[47]:
 
 
 import pyodbc
@@ -98,95 +127,92 @@ print('Conexão Bem sucedida')
 #cursor.close()
 
 
-# In[10]:
+# In[36]:
 
 
 # Inserindo os pontos de interesse e cadastrando esses pontos no Banco de Dados:
 
-cursor = connection.cursor()
-
 print('Insira as informações dos pontos de interesse que deseja cadastrar.')
-    
+
 while True:    
     name = str(input('Insira o nome do ponto de interesse. Para cancelar o resgitro de um novo ponto, basta apertar enter com a caixa vazia:'))
     if name == '':
         break        
     
     try:
-        x = int(input('Insira a coordenada X do ponto de interesse:'))    
-        y = int(input('Insira a coordenada Y do ponto de interesse:'))
-        print(x,y)
+        x = int(input('Insira a coordenada X do ponto de interesse:'))
         if x < 0:
             print('Coordenada X inválida!')
-        elif y < 0:
+            continue
+        
+        y = int(input('Insira a coordenada Y do ponto de interesse:'))
+        if y < 0:
             print('Coordenada Y inválida!')
-        else:        
-            cursor.execute("INSERT INTO POI (NAME, X, Y) VALUES (?, ?, ?)", (name, x, y))
-            cursor.commit()
+            continue
+                     
+        insert_pois(connection, Poi(name, x, y))
+#        insert_pois(connection, create_poi(name, x, y))
+#    except Exception as error:
     except:
+#        print(error)
         print('Valor digitado inválido!')
         
 # Capturando os pontos cadastrados no Banco de Dados para uso posterior:
 
-cursor.execute("SELECT * FROM POI") 
-row = cursor.fetchall()
-print(row)        
-cursor.close()
+print(get_pois(connection))        
 
 
-# In[11]:
+# In[64]:
 
 
 # Listando os pontos de interesse:
 
-for key, name_poi, coordx, coordy in row:
-    print('{} (x = {}, y = {})'.format(name_poi, coordx,coordy))
+for poi in get_pois(connection):
+    print('* {}\n'.format(poi))
+
+# for poi in get_pois(connection):
+#     print('* {}\n'.format(format_poi(poi)))
 
 
-# In[5]:
+# In[65]:
 
 
 # Inserindo o ponto de referência e a distância máxima:
 
-pontoreferencia = []
+reference_point = {}
 
 print('Insira as informações do ponto de referência.')
-pontoreferencia.append(int(input('Insira a coordenada X do ponto de referência:')))
-pontoreferencia.append(int(input('Insira a coordenada Y do ponto de referência:')))
-pontoreferencia.append(int(input('Insira a distância maxima (em metros) do ponto de referência:')))
-print(pontoreferencia)
+reference_point['x'] = (int(input('Insira a coordenada X do ponto de referência:')))
+reference_point['y'] = (int(input('Insira a coordenada Y do ponto de referência:')))
+reference_point['max_distance'] = (int(input('Insira a distância maxima (em metros) do ponto de referência:')))
+print(reference_point)
 
 
-# In[6]:
+# In[69]:
 
-
-from math import sqrt
 
 # Calculando as distância entre pontos e listando os pontos de interesse por proximidade:
 
-for key, name_poi, coordx, coordy in row:
-    dist = sqrt(((pontoreferencia[0] - coordx)**2) + ((pontoreferencia[1] - coordy)**2))
-    if dist <= pontoreferencia[2]:
-        print(name_poi)
+dict_distance = {}
+
+for poi in get_pois(connection):
+    distance = poi.calc_distance(reference_point['x'], reference_point['y'])
+#   distance = calc_distance(reference_point['x'], reference_point['y'], poi['x'], poi['y']) 
+    if distance <= reference_point['max_distance']:
+        print(poi.name)
+#        print(poi['name'])
+        dict_distance[distance] = poi
+#        dict_distance[distance] = poi['name']    
 
 
-# In[7]:
+# In[74]:
 
-
-from math import sqrt
 
 # Calculando as distância entre pontos e listando os pontos de interesse por proximidade (em ordem crescente):
 
-dict_dist = {}
-
-for key, name_poi, coordx, coordy in row:
-    dist = sqrt(((pontoreferencia[0] - coordx)**2) + ((pontoreferencia[1] - coordy)**2))
-    if dist <= pontoreferencia[2]:
-        dict_dist[dist] = name_poi
-#print(dict_dist)
-
 i = 1 
-for key in sorted(dict_dist.keys()) :
-    print('{}. {} - distância: {:.2f} metros'.format(i, dict_dist[key], key))
+for key in sorted(dict_distance.keys()) :
+    print('{}. {} - distância: {:.2f} metros'.format(i, dict_distance[key].name, key))
+#    print('{}. {} - distância: {:.2f} metros'.format(i, dict_distance[key], key))    
     i += 1
 
